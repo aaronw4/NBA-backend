1830153912a694fa4b51ab6b381f709e6924caca
Aaron Williams <aaronrw4@yahoo.com>
2021-05-15 17:47:04 -0700

    Created database

---
+++ data/database.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+conn = sqlite3.connect('NBA.db')
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS teams (
+    id integer PRIMARY KEY,
+    city text NOT NULL,
+    name text NOT NULL
+) """)
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS stats (
+    [id] integer PRIMARY KEY,
+    [date] text NOT NULL,
+    [points] integer NOT NULL,
+    [points_opp] integer NOT NULL,
+    [fg] integer NOT NULL,
+    [fg_opp] integer NOT NULL,
+    [fga] integer NOT NULL,
+    [fga_opp] integer NOT NULL,
+    [to] integer NOT NULL,
+    [to_opp] integer NOT NULL,
+    [fta] integer NOT NULL,
+    [fta_opp] integer NOT NULL,
+    [drb] integer NOT NULL,
+    [drb_opp] NOT NULL, 
+    [orb] integer NOT NULL,
+    [orb_opp] integer NOT NULL,
+    [three] integer NOT NULL,
+    [three_opp] integer NOT NULL,
+    [team_id] integer NOT NULL,
+    FOREIGN KEY ("team_id") REFERENCES teams (id)
+) """)
+
+conn.commit()

bc2781e7603e29c44db2665f0ad9a74c47a09294
Aaron Williams <aaronrw4@yahoo.com>
2021-05-23 17:17:22 -0700

    Modified to return a json file

--- seasonStats.py
+++ seasonStats.py
@@ -1,17 +1,27 @@
 import sqlite3
+import json
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx,col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 conn = sqlite3.connect('NBA.db')
+conn.row_factory = dict_factory
 cursor = conn.cursor()
 
 cursor.execute(
-    '''SELECT teams.name, avg(stats.points), avg(stats.points_opp), 
-    avg(stats.fg), avg(stats.fg_opp), avg(stats.fga), avg(stats.fga_opp),
-    avg(stats.tov), avg(stats.tov_opp), avg(stats.fta), avg(stats.fta_opp), 
-    avg(stats.drb), avg(stats.drb_opp), avg(stats.orb), avg(stats.orb_opp),
-    avg(stats.three), avg(stats.three_opp), avg(stats.three_a), avg(stats.three_opp_a)
+    '''SELECT teams.name, avg(stats.points) as points, avg(stats.points_opp) as points_opp, 
+    avg(stats.fg) as fg, avg(stats.fg_opp) as fg_opp, avg(stats.fga) as fga, avg(stats.fga_opp) as fga_opp,
+    avg(stats.tov) as tov, avg(stats.tov_opp) as tov_opp, avg(stats.fta) as fta, avg(stats.fta_opp) as fta_opp, 
+    avg(stats.drb) as drb, avg(stats.drb_opp) as drb_opp, avg(stats.orb) as orb, avg(stats.orb_opp) as orb_opp,
+    avg(stats.three) as three, avg(stats.three_opp) as three_opp, avg(stats.three_a) as three_a, avg(stats.three_opp_a) as three_a_opp
     FROM teams INNER JOIN stats ON teams.id = stats.team_id
     GROUP BY teams.name'''
 )
 
 result = cursor.fetchall()
-print(result)
+data = json.dumps(result)
+
+print(data)

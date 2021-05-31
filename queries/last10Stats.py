bccfbcd6d4fb5aa96bff052d3cd1309a7e759ac4
Aaron Williams <aaronrw4@yahoo.com>
2021-05-31 16:45:35 -0700

    Reformatted as functions. Removed import json

--- queries/last10Stats.py
+++ queries/last10Stats.py
@@ -1,56 +1,52 @@
 import sqlite3
-import json
 
-date = "2021-04-01"
+def last10Stats(date):
+    def dict_factory(cursor, row):
+        d = {}
+        for idx,col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
 
-def dict_factory(cursor, row):
-    d = {}
-    for idx,col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
+    conn = sqlite3.connect('NBA.db')
+    conn.row_factory = dict_factory
+    cursor = conn.cursor()
 
-conn = sqlite3.connect('NBA.db')
-conn.row_factory = dict_factory
-cursor = conn.cursor()
+    cursor.execute(
+        '''
+        SELECT teams.name, stats.date, 
+        avg(stats.points) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as points, 
+        avg(stats.points_opp) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as points_opp, 
+        avg(stats.fg) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as fg, 
+        avg(stats.fg_opp) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as fg_opp, 
+        avg(stats.fga) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as fga, 
+        avg(stats.fga_opp) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as fga_opp, 
+        avg(stats.tov) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as tov, 
+        avg(stats.tov_opp) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as tov_opp, 
+        avg(stats.fta) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as fta, 
+        avg(stats.fta_opp) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as fta_opp, 
+        avg(stats.drb) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as drb, 
+        avg(stats.drb_opp) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as drb_opp, 
+        avg(stats.orb) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as orb, 
+        avg(stats.orb_opp) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as orb_opp, 
+        avg(stats.three) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as three, 
+        avg(stats.three_opp) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as three_opp, 
+        avg(stats.three_a) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as three_a, 
+        avg(stats.three_opp_a) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as three_a_opp
+        FROM teams INNER JOIN stats ON teams.id = stats.team_id
+        WHERE stats.date <:date
+        ''', ({"date":date})
+    )
 
-cursor.execute(
-    '''
-    SELECT teams.name, stats.date, 
-    avg(stats.points) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as points, 
-    avg(stats.points_opp) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as points_opp, 
-    avg(stats.fg) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as fg, 
-    avg(stats.fg_opp) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as fg_opp, 
-    avg(stats.fga) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as fga, 
-    avg(stats.fga_opp) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as fga_opp, 
-    avg(stats.tov) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as tov, 
-    avg(stats.tov_opp) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as tov_opp, 
-    avg(stats.fta) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as fta, 
-    avg(stats.fta_opp) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as fta_opp, 
-    avg(stats.drb) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as drb, 
-    avg(stats.drb_opp) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as drb_opp, 
-    avg(stats.orb) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as orb, 
-    avg(stats.orb_opp) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as orb_opp, 
-    avg(stats.three) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as three, 
-    avg(stats.three_opp) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as three_opp, 
-    avg(stats.three_a) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as three_a, 
-    avg(stats.three_opp_a) OVER(PARTITION BY teams.name ORDER BY stats.date ROWS BETWEEN 9 PRECEDING and CURRENT ROW) as three_a_opp
-    FROM teams INNER JOIN stats ON teams.id = stats.team_id
-    WHERE stats.date <:date
-    ''', ({"date":date})
-)
+    results = cursor.fetchall()
 
-results = cursor.fetchall()
+    last10 = []
 
-last10 = []
+    for index in range(len(results)):
+        if index == len(results)-1:
+            last10.append(results[index])
+        elif results[index]['date'] < results[index+1]['date']:
+            continue
+        else:
+            last10.append(results[index])
 
-for index in range(len(results)):
-    if index == len(results)-1:
-        last10.append(results[index])
-    elif results[index]['date'] < results[index+1]['date']:
-        continue
-    else:
-        last10.append(results[index])
-        
-data = json.dumps(last10)
-
-print(data)
+    return last10
--- queries/seasonStats.py
+++ queries/seasonStats.py
@@ -1,29 +1,26 @@
 import sqlite3
-import json
 
-date = "2021-05-01"
+def seasonStats(date):
+    def dict_factory(cursor, row):
+        d = {}
+        for idx,col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
 
-def dict_factory(cursor, row):
-    d = {}
-    for idx,col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
+    conn = sqlite3.connect('NBA.db')
+    conn.row_factory = dict_factory
+    cursor = conn.cursor()
 
-conn = sqlite3.connect('NBA.db')
-conn.row_factory = dict_factory
-cursor = conn.cursor()
+    cursor.execute(
+        '''SELECT teams.name, avg(stats.points) as points, avg(stats.points_opp) as points_opp, 
+        avg(stats.fg) as fg, avg(stats.fg_opp) as fg_opp, avg(stats.fga) as fga, avg(stats.fga_opp) as fga_opp,
+        avg(stats.tov) as tov, avg(stats.tov_opp) as tov_opp, avg(stats.fta) as fta, avg(stats.fta_opp) as fta_opp, 
+        avg(stats.drb) as drb, avg(stats.drb_opp) as drb_opp, avg(stats.orb) as orb, avg(stats.orb_opp) as orb_opp,
+        avg(stats.three) as three, avg(stats.three_opp) as three_opp, avg(stats.three_a) as three_a, avg(stats.three_opp_a) as three_a_opp
+        FROM teams INNER JOIN stats ON teams.id = stats.team_id
+        WHERE stats.date <:date GROUP BY teams.name''', ({"date":date})
+    )
 
-cursor.execute(
-    '''SELECT teams.name, avg(stats.points) as points, avg(stats.points_opp) as points_opp, 
-    avg(stats.fg) as fg, avg(stats.fg_opp) as fg_opp, avg(stats.fga) as fga, avg(stats.fga_opp) as fga_opp,
-    avg(stats.tov) as tov, avg(stats.tov_opp) as tov_opp, avg(stats.fta) as fta, avg(stats.fta_opp) as fta_opp, 
-    avg(stats.drb) as drb, avg(stats.drb_opp) as drb_opp, avg(stats.orb) as orb, avg(stats.orb_opp) as orb_opp,
-    avg(stats.three) as three, avg(stats.three_opp) as three_opp, avg(stats.three_a) as three_a, avg(stats.three_opp_a) as three_a_opp
-    FROM teams INNER JOIN stats ON teams.id = stats.team_id
-    WHERE stats.date <:date GROUP BY teams.name''', ({"date":date})
-)
+    result = cursor.fetchall()
 
-result = cursor.fetchall()
-data = json.dumps(result)
-
-print(data)
+    return result

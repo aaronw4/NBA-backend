1829c37c0c6c002e59a57a6c0398198164821bf7
Aaron Williams <aaronrw4@yahoo.com>
2021-07-25 15:37:23 -0700

    Created changeName function

---
+++ data/changeName.py
@@ -0,0 +1,62 @@
+def changeName(city):
+    if city == 'Atlanta':
+        name = 'Hawks'
+    elif city == 'Boston':
+        name = 'Celtics' 
+    elif city == 'Brooklyn':
+        name = 'Nets'
+    elif city == 'Charlotte':
+        name ='Hornets'
+    elif city == 'Chicago':
+        name = 'Bulls'
+    elif city == 'Cleveland':
+        name = 'Cavaliers'
+    elif city == 'Dallas':
+        name = 'Mavericks'
+    elif city == 'Denver':
+        name = 'Nuggets'
+    elif city == 'Detroit':
+        name = 'Pistons'
+    elif city == 'Golden State':
+        name = 'Warriors'
+    elif city == 'Houston':
+        name = 'Rockets'
+    elif city == 'Indiana':
+        name = 'Pacers'
+    elif city == 'L.A. Clippers':
+        name = 'Clippers'
+    elif city == 'L.A. Lakers':
+        name = 'Lakers'
+    elif city == 'Memphis':
+        name = 'Grizzlies'
+    elif city == 'Miami':
+        name = 'Heat'
+    elif city == 'Milwaukee':
+        name = 'Bucks'
+    elif city == 'Minnesota':
+        name = 'Timberwolves'
+    elif city == 'New Orleans':
+        name = 'Pelicans'
+    elif city == 'New York':
+        name = 'Knicks'
+    elif city == 'Oklahoma City':
+        name = 'Thunder'
+    elif city == 'Orlando':
+        name = 'Magic'
+    elif city == 'Philadelphia':
+        name = '76ers'
+    elif city == 'Phoenix':
+        name = 'Suns'
+    elif city == 'Portland':
+        name = 'Trail Blazers'
+    elif city == 'Sacramento':
+        name = 'Kings'
+    elif city == 'San Antonio':
+        name = 'Spurs'
+    elif city == 'Toronto':
+        name = 'Raptors'
+    elif city == 'Utah':
+        name = 'Jazz'
+    elif city == 'Washington':
+        name = 'Wizards'
+    return name
